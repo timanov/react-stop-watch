f82f0646a65557792bcf5d9ebf972fb23df54564
mikhail <mihailtimanov@gmail.com>
2022-09-25 15:21:22 +0300

    Создание двух компонентов

---
+++ src/Components/BtnDisplayComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function BtnDisplayComponent() {
+  return (
+    <div className="main-section">
+      <div className='clock-holder'>
+          <div className='stopwatch'>
+            
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default BtnDisplayComponent;
---
+++ src/Components/DisplayComponent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function DisplayComponent() {
+  return (
+    <div className="main-section">
+      <div className='clock-holder'>
+          <div className='stopwatch'>
+            
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default DisplayComponent;
