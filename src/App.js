282edcf84c4242a831ebfcbdc9c649221920f76b
mikhail <mihailtimanov@gmail.com>
2022-09-25 16:26:41 +0300

    Минимальная работа секундомера

--- src/App.js
+++ src/App.js
@@ -5,12 +5,36 @@ import './App.css';
 
 function App() {
   const [time, setTime] = useState({ms:0, s:0, m:0, h:0});
+
+  const start = () => {
+    run();
+    setInterval(run, 10);
+  }
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 99){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM, h:updatedH});
+  };
   return (
     <div className="main-section">
       <div className='clock-holder'>
           <div className='stopwatch'>
             <DisplayComponent time ={time}/>
-            <BtnComponent />
+            <BtnComponent start={start}/>
           </div>
       </div>
     </div>
--- src/Components/BtnComponent.js
+++ src/Components/BtnComponent.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-function BtnComponent() {
+function BtnComponent(props) {
   return (
     <div>
-      <button className='stopwatch-btn-gre'>Start</button>
+      <button className='stopwatch-btn stopwatch-btn-gre'
+      onClick={props.start}>Start</button>
     </div>
   );
 }
